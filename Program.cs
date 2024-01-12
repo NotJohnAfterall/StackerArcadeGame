f9a048e26f390e91478dbcbaa51782b4c12816ee
NotJohnAfterall <145935737+NotJohnAfterall@users.noreply.github.com>
2024-01-12 11:13:09 +0100

    adsas

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ namespace PVA_Game
     internal class Program
     {
         //TODO: make when space is pressed the mover stops moving
-        
-        
-        
+
+
+
         static public char[,] displayArray = new char[10, 11];
         static public char[,] systemArray = new char[10, 11];
         static public int moverPos = 2;
@@ -31,113 +31,176 @@ namespace PVA_Game
         static public int level7moverPos = 0;
         static public int level8moverPos = 0;
         static public int freezeMoverPos = 0;
-        static public int hashtagIndex = 0;
+        static public int freezeMoverPosBefore = 0;
+        static public int hashtagIndexX = 0;
+        static public int hashtagIndexY = 0;
+        static public bool didlevelChanged = false;
+        static public int levelBefore = 0;
+        static public bool alreadypassed = false;
 
 
 
         static void Main(string[] args)
         {
-            
-            
+            didlevelChanged = true;
+
             while (true)
             {
+                Console.WriteLine(didlevelChanged);
                 SpaceCounter();
+                Console.WriteLine($"Variables {spacebarPressedTimes} {levelBefore}");
                 if (spacebarPressedTimes == 0)
                 {
+                    Console.WriteLine("space = 0");
                     SetBordersInArray();
-                    
+
                     MoverMove();
                     Thread.Sleep(100);
                     Console.Clear();
                 }
-                else 
+                else
                 {
-                    SetBordersInArray();                    
-                    
-                    if (levelChanged == 0)
+                    SetBordersInArray();
+                    if (spacebarPressedTimes > levelBefore)
                     {
-                        levelChanged++;
-                        afterSpacePress();
-                        
-                        
-                        
+                        Console.WriteLine($"LEVEL CHANGE TRIGERED {spacebarPressedTimes} {levelChanged}");
+                        if (spacebarPressedTimes > 8) { spacebarPressedTimes = 8; }
+                        else { levelChanged++; freezeMoverPos = moverPos; }
+
+
+                        //Thread.Sleep(1000);
+                        didlevelChanged = false;
+
+
+
+
                     }
-                    
+                    else
+                    {
+                        Console.WriteLine("else");
+                    }
+
                     MoverMove();
-                    
+
                     Thread.Sleep(100);
                     Console.Clear();
 
-                    
-                    
+
+
                 }
-                
-                
+
+
             }
-            
-            
 
 
+
+
+        }
+
+        static void stackChecker()
+        {
+            if (freezeMoverPos > freezeMoverPosBefore)
+            {
+                if (freezeMoverPos == freezeMoverPosBefore - 1)
+                {
+                    displayArray
+                }
+            }
         }
 
         static void SpaceCounter()
         {
-            
-                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
-                {
-                        spacebarPressedTimes++;
-                }
-               
-            
-        }
 
-        static void printPrevious()
-        {
-            
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+            {
+                spacebarPressedTimes++;
+            }
+
 
         }
+
         static void MoverMove()
         {
+            if (spacebarPressedTimes <= 7)
+            {
+                displayArray[displayArray.GetLength(0) - 2 - spacebarPressedTimes, moverPos] = 'O';
+            }
             
-            displayArray[displayArray.GetLength(0) - 2 - spacebarPressedTimes, moverPos] = 'O';
+
             
-                if (spacebarPressedTimes >= 7)
-                {
-                    spacebarPressedTimes = 7;
-                }
-                switch (levelChanged)
-                    {
-                        case 1:
-                            level1moverPos = freezeMoverPos;
-                            printFreezed(freezeMoverPos, levelChanged);
-                            
-                            displayArray[displayArray.GetLength(0) - (1 + levelChanged), level1moverPos] = '#';
-                            break;
-                        case 2:
-                            level2moverPos = freezeMoverPos;
-                            displayArray[displayArray.GetLength(0) - (1 + levelChanged), level1moverPos] = '#';
-                            break;
-                        case 3:
-                            level3moverPos = freezeMoverPos;
-                            break;
-                        case 4:
-                            level4moverPos = freezeMoverPos;
-                            break;
-                        case 5:
-                            level5moverPos = freezeMoverPos;
-                            break;
-                        case 6:
-                            level6moverPos = freezeMoverPos;
-                            break;
-                        case 7:
-                            level7moverPos = freezeMoverPos;
-                            break;
-                        case 8:
-                            level8moverPos = freezeMoverPos;
-                            break;
-                    
-                }
-            GetHashtagIndex(displayArray);
+            switch (levelChanged)
+            {
+                case 1:
+                    level1moverPos = freezeMoverPos;
+                    print(level1moverPos, levelChanged);
+                    levelBefore = 1;
+                    break;
+                case 2:
+                    level2moverPos = freezeMoverPos; freezeMoverPosBefore = level1moverPos;
+                    print(level1moverPos, levelChanged-1);
+                    print(level2moverPos, levelChanged);
+                    levelBefore =2;
+                    break;
+                case 3:
+                    level3moverPos = freezeMoverPos; freezeMoverPosBefore = level2moverPos;
+                    print(level1moverPos, levelChanged - 2);
+                    print(level2moverPos, levelChanged - 1);
+                    print(level3moverPos, levelChanged);
+                    levelBefore = 3;
+                    break;
+                case 4:
+                    level4moverPos = freezeMoverPos; freezeMoverPosBefore = level3moverPos;
+                    print(level1moverPos, levelChanged - 3);
+                    print(level2moverPos, levelChanged - 2);
+                    print(level3moverPos, levelChanged - 1);
+                    print(level4moverPos, levelChanged);
+                    levelBefore = 4;
+                    break;
+                case 5:
+                    level5moverPos = freezeMoverPos; freezeMoverPosBefore = level3moverPos;
+                    print(level1moverPos, levelChanged - 4);
+                    print(level2moverPos, levelChanged - 3);
+                    print(level3moverPos, levelChanged - 2);
+                    print(level4moverPos, levelChanged - 1);
+                    print(level5moverPos, levelChanged);
+                    levelBefore = 5;
+                    break;
+                case 6:
+                    level6moverPos = freezeMoverPos; freezeMoverPosBefore = level4moverPos;
+                    print(level1moverPos, levelChanged - 5);
+                    print(level2moverPos, levelChanged - 4);
+                    print(level3moverPos, levelChanged - 3);
+                    print(level4moverPos, levelChanged - 2);
+                    print(level5moverPos, levelChanged - 1);
+                    print(level6moverPos, levelChanged);
+                    levelBefore = 6;
+                    break;
+                case 7:
+                    level7moverPos = freezeMoverPos; freezeMoverPosBefore = level5moverPos;
+                    print(level1moverPos, levelChanged - 6);
+                    print(level2moverPos, levelChanged - 5);
+                    print(level3moverPos, levelChanged - 4);
+                    print(level4moverPos, levelChanged - 3);
+                    print(level5moverPos, levelChanged - 2);
+                    print(level6moverPos, levelChanged - 1);
+                    print(level7moverPos, levelChanged);
+                    levelBefore = 7;
+                    break;
+                case 8:
+                    level8moverPos = freezeMoverPos;
+                    print(level1moverPos, levelChanged - 7);
+                    print(level2moverPos, levelChanged - 6);
+                    print(level3moverPos, levelChanged - 5);
+                    print(level4moverPos, levelChanged - 4);
+                    print(level5moverPos, levelChanged - 3);
+                    print(level6moverPos, levelChanged - 2);
+                    print(level7moverPos, levelChanged - 1);
+                    print(level8moverPos, levelChanged);
+                    levelBefore = 8;
+                    break;
+
+            }
+            
             foreach (var item in displayArray)
             {
                 if (item == 'O')
@@ -147,17 +210,16 @@ namespace PVA_Game
                 }
                 else if (item == '#')
                 {
-                    displayArray[displayArray.GetLength(0) - (1 + levelChanged), hashtagIndex - 1] = '#';
-                    displayArray[displayArray.GetLength(0) - (1 + levelChanged), hashtagIndex + 1] = '#';
+                    
                 }
             }
 
-            
+
             while (true)
             {
                 if (moverPos == displayArray.GetLength(1) - 3 || (moverPos < moverPosBefore && moverPosBefore != 3))
                 {
-                    
+
                     moverPosBefore = moverPos;
                     moverPos--;
                     Display();
@@ -177,7 +239,7 @@ namespace PVA_Game
 
                 }
             }
-            
+
         }
         static void SetBordersInArray()
         {
@@ -212,33 +274,13 @@ namespace PVA_Game
                 Console.WriteLine();
             }
         }
-          
-        static void afterSpacePress()
-        {
 
-            freezeMoverPos = moverPos;
-            displayArray[displayArray.GetLength(0) - (1 + levelChanged) , freezeMoverPos] = '#';
-            printPrevious();
+        static void print(int x, int y)
+        {
+            displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
+            displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
+            displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
         }
 
-        static void printFreezed(int xypos, int Lvlheight)
-        {
-            
-        }
-        static void GetHashtagIndex(char[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    if (array[i, j] == '#')
-                    {
-                        hashtagIndex = j;
-                    }
-                }
-            }
-            
-        }
-        
     }
 }
