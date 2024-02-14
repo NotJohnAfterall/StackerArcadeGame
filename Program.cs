85f4eb6aeda3f6750f5862399d3229d9735a60b8
NotJohnAfterall <145935737+NotJohnAfterall@users.noreply.github.com>
2024-02-14 10:00:25 +0100

    Code optimalizations

--- Program.cs
+++ Program.cs
@@ -13,25 +13,17 @@ namespace PVA_Game
     internal class Program
     {
         static public char[,] displayArray = new char[10, 11];
-        static public char[,] systemArray = new char[10, 11];
         static public int moverPos = 2;
         static public int moverPosBefore = 0;
         static public int spacebarPressedTimes = 0;
         static public int levelChanged = 0;
         static public int freezeMoverPos = 0;
         static public int freezeMoverPosBefore = 0;
-        static public int hashtagIndexX = 0;
-        static public int hashtagIndexY = 0;
         static public int levelBefore = 0;
-        static public bool alreadypassed = false;
         static public bool stackAproved = false;
         static public int[] freezedLevelPoses = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         static public bool stackingChecked = false;
-        static public int moverState = 0;
-        static public int[] levelsPrintType = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         static public int randomNumber = 100;
-
-        //Mover states 0 = normal, 1 = 2x on right, 2 = 1x on right, -1 = 2x on left, -2 = 1x on left
 
         static void Main(string[] args)
         {
@@ -165,76 +157,14 @@ namespace PVA_Game
             {
                 displayArray[displayArray.GetLength(0) - 2 - spacebarPressedTimes, moverPos] = 'O';
             }
-            
-            switch (levelChanged)
-            {
-                case 1:
-                    freezedLevelPoses[1] = freezeMoverPos; 
-                    for (int i = 1; i <= 1; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (1 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 1;
-                    break;
-                case 2:
-                    freezedLevelPoses[2] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[1];
-                    for (int i = 1; i <= 2; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (2 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 2;
-                    break;
-                case 3:
-                    freezedLevelPoses[3] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[2];
-                    for (int i = 1; i <= 3; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (3 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 3;
-                    break;
-                case 4:
-                    freezedLevelPoses[4] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[3];
-                    for (int i = 1; i <= 4; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (4 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 4;
-                    break;
-                case 5:
-                    freezedLevelPoses[5] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[3];
-                    for (int i = 1; i <= 5; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (5 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 5;
-                    break;
-                case 6:
-                    freezedLevelPoses[6] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[4];
-                    for (int i = 1; i <= 6; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (6 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 6;
-                    break;
-                case 7:
-                    freezedLevelPoses[7] = freezeMoverPos; freezeMoverPosBefore = freezedLevelPoses[5];
-                    for (int i = 1; i <= 7; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (7 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 7;
-                    break;
-                case 8:
-                    freezedLevelPoses[8] = freezeMoverPos;
-                    for (int i = 1; i <= 8; i++)
-                    {
-                        print(freezedLevelPoses[i], levelChanged - (8 - i), levelsPrintType[i]);
-                    }
-                    levelBefore = 8;
-                    break;
 
+            freezedLevelPoses[levelChanged] = freezeMoverPos;
+            for (int i = 1; i <= levelChanged; i++)
+            {
+                print(freezedLevelPoses[i], levelChanged - (levelChanged - i));   
             }
-            
+            levelBefore = levelChanged;
+
             foreach (var item in displayArray)
             {
                 if (item == 'O')
@@ -317,36 +247,11 @@ namespace PVA_Game
             }
         }
 
-        static void print(int x, int y, int printtype)
+        static void print(int x, int y)
         {
-            switch (printtype)
-            {
-                case 0:
-                    displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
-                    displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
-                    displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
-                    break;
-                case 1:
-                    displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
-                    displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
-                    break;
-                case 2:
-                    displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
-                    break;
-                case -1:
-                    displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
-                    displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
-                    break;
-                case -2:
-                    displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
-                    break;
-            }
-            //if (printType == 0)
-            //{
-            //    displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
-            //    displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
-            //    displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
-            //}
+            displayArray[displayArray.GetLength(0) - (1 + y), x] = '#';
+            displayArray[displayArray.GetLength(0) - (1 + y), x - 1] = '#';
+            displayArray[displayArray.GetLength(0) - (1 + y), x + 1] = '#';
         }
     }
 }
